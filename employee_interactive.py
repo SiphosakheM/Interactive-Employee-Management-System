eb93d09093cbc8add08d7636fa362f09ddee8cbb
simsieec025 <simsieec025@student.wethinkcode.co.za>
2025-12-25 19:15:21 +0200

    fixing the bug i was suppose to pass in the person as an argument in the class employee and put in break on the while loop also put super to call the instance attribute from the class person

--- employee_interactive.py
+++ employee_interactive.py
@@ -5,15 +5,16 @@ class Person:
         self.email = email
     
     def get_details(self):
-        print(self.name, self.surname, self.email)
+        print(self.name, self.surname, self.email, sep="\n")
 
-class Employee:
-    def __init__(self, employee_id, department):
+class Employee(Person):
+    def __init__(self, name, surname, email, employee_id, department):
+        super().__init__(name, surname, email)
         self.employee_id = employee_id
         self.department = department
     
     def get_details(self):
-        print(self.name, self.surname, self.email, self.employee_id, self.department)
+        print(self.name, self.surname, self.email, self.employee_id, self.department, sep="\n")
     
 my_entries = []
 
@@ -23,19 +24,20 @@ while True:
         if choice == "q":
             break
         
-        name = input("Enter your Name: ")
-        surname = input("Enter your Surname: ")
-        email = input("Enter your Email: ")
+        name = input("Enter your Name: ").strip().title()
+        surname = input("Enter your Surname: ").strip().title()
+        email = input("Enter your Email: ").strip()
         
         if choice == "1":
             my_entries.append(Person(name, surname, email))
         elif choice == "2":
-            employee_id = input("Enter your Employee ID: ")
-            department = input("Enter your Department: ") 
+            employee_id = input("Enter your Employee ID: ").strip()
+            department = input("Enter your Department: ").strip().capitalize()
             my_entries.append(Employee(name, surname, email, employee_id, department))
+            break
     except ValueError:
         print("incomplet infomation")
         
 print("\n ---Final Report ---")
 for entry in my_entries:
-    print(entry.get_details())
+    entry.get_details()
